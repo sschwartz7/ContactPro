9e5fbc3ae01ffae46c6fb3cf9aed9e42467e3302
Simon Schwartz <sschwartz794@gmail.com>
2023-10-16 11:28:32 -0500

    Added "asp-for" in edit categories input

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -105,7 +105,7 @@ namespace ContactPro.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,Name")] Category category)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,Name,AppUser")] Category category)
         {
             if (id != category.Id)
             {
